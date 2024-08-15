ca9df198a03e8fe23a5f6cc2a108117cf81cfc14
Abhishek Rathnayaka <harindu@navitsa.com>
2024-08-15 13:32:08 +0530

    update navbar/slidebar and footer

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/NavBar/NavBar';
 import Footer from "./components/Footer/Footer";
 import SideBar from "./components/SideBar/SideBar";
-// import Home from './pages/Home';
-// import Contact from './pages/Contact';
-// import Appointments from './pages/Appointments';
-// import About from './pages/About';
-// import Signup from './pages/Signup';
-// import Login from './pages/Login';
-// import Notifications from './pages/Notifications';
+import AppRoute from "./App.route";
+
 
 function App() {
   return (
       <Router>
-        <div>
-          <Navbar />
-            <SideBar/>
-          <Footer/>
-        </div>
+        <AppRoute />
       </Router>
   );
 }
