96b9ccbf2514ef7a50d4f66c0bc894ebd40bc1c8
sadulafernando <sadulatharusha@gmail.com>
2024-09-05 01:25:02 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/NavBar/NavBar';
 import Footer from "./components/Footer/Footer";
 import SideBar from "./components/SideBar/SideBar";
 import AppRoute from "./App.route";
+import PaymentDetailsPopup from './Pages/PaymentDetails/PaymentDetailPopup';
+import AppointmentTable from './components/AppointmentTable';
 
+const App = () => {
+  const [showPopup, setShowPopup] = useState(false);
 
-function App() {
   return (
-      <Router>
-        <AppRoute />
-      </Router>
+    <Router>
+      <Navbar />
+      <AppointmentTable onRowClick={() => setShowPopup(true)} /> 
+      <PaymentDetailsPopup show={showPopup} onClose={() => setShowPopup(false)} />
+      <Footer />
+    </Router>
   );
 }
 
 export default App;
+
+
